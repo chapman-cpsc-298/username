76c208c55b4731df21a5b2cb4566dc416481ca70
Sean Lindsey <slindsey@chapman.edu>
2019-11-06 10:46:31 -0800

    Adds Username.sh

---
+++ username.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# zip.sh
+# author
+echo "Enter a five-digit ZIP code: "
+read ZIP
+while echo "$USERNAME" | egrep -v "^[0-9][a-z][~/dev/null 2>&1
+do
+	echo "You must enter a valid ZIP code - five digits only!"
+	echo "Enter a five-digit ZIP code: "
+	read ZIP
+done
+echo "Thank you"
+
