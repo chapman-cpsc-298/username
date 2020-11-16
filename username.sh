9a9645a220d629ade6585b6c396783cf746a995f
Steven Abe <sabe@chapman.edu>
2020-11-15 18:06:41 -1000

    50th try

--- username.sh
+++ username.sh
@@ -7,10 +7,10 @@ echo "It must start with a lowercase character"
 echo "It must contain at least 3 but not more than 12 characters."
 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username"
+	echo "Enter a username: "
 	read USERNAME
 done
 echo "Thank you"
